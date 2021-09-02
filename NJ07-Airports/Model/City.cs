876c030b0d337d59385cfcb352d2e170d605c526
Belian <radoszabelian@gmail.com>
2021-09-02 10:47:44 +0200

    Add project files.

---
+++ NJ07-Airports/Model/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJ07_Airports.Model
+{
+    public class City
+    {
+        public int CountryId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TimeZoneName { get; set; }
+    }
+}
---
+++ NJ07-Airports/Model/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJ07_Airports.Model
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ThreeLetterISOCode { get; set; }
+        public string TwoLetterISOCode { get; set; }
+    }
+}
