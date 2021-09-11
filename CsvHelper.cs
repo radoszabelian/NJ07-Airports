8352b800e1c43cd766f8664ace4ea7293b94d36b
v-bradosza <v-bradosza@expediagroup.com>
2021-09-11 13:57:46 +0200

    csv helper skeleton

---
+++ CsvHelper.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace NJ07_Airports {
+    public static class CsvHelper {
+        public static List<MODELTYPE> Parse<MODELTYPE>(string fileName) {
+
+        }
+    }
+}
